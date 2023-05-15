9a54e1fff51c004c8bf870ac54aa38a94290ecf9
Ronan Pardo Soares <ronan.soares@gmail.com>
2023-05-15 15:59:05 -0300

    Fix change of tests

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 valor = input('Digite um valor: ')
 inteiro = int(valor)
 if inteiro < 0:
-    print("impossível!")
+    print("Impossível!")
 elif inteiro < 18:
-    print('não precisa se alistar.')
+    print('Não precisa se alistar.')
 elif 18 < inteiro < 65:
     print('Não esqueça de votar na próxima eleição.')
 elif 65 < inteiro:
     print('Vá descansar.')
 else:
-    print('eita!')
+    print('Eita!')
